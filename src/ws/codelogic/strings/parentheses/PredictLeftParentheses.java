4e8b6423901f46fe5432653e143df3e676bf65cb
codelogicws <russ@codelogic.ws>
2015-05-23 18:32:39 -0700

    more progress on the left parentheses predictor.

--- src/ws/codelogic/strings/parentheses/PredictLeftParentheses.java
+++ src/ws/codelogic/strings/parentheses/PredictLeftParentheses.java
@@ -1,25 +1,48 @@
 package ws.codelogic.strings.parentheses;
 
+import ws.codelogic.collections.stack.Stack;
+import ws.codelogic.collections.stack.StackFactory;
+
 public class PredictLeftParentheses {
 
     private final static int PARENTHESESPAIRMAX = 20000;
+    private Stack<Character> stack;
+    private int rightParenthesesCount;
 
     public String insertLeft(String s){
-        int rightParenthesesCount = 0;
+        rightParenthesesCount = 0;
+        stack = StackFactory.make(PARENTHESESPAIRMAX);
         for(int i=s.length()-1;i>=0;i--){
             char current = s.charAt(i);
-            if(current == '*' || current == '/'){
-                s = s.substring(0, i+1) + '(' + s.substring(i+1);
+            if(isStarOrForwardSlash(current)){
+                stack.push('(');
                 --rightParenthesesCount;
-            }
-            if(current == ')')rightParenthesesCount++;
+            }else if(current == ')')
+                rightParenthesesCount++;
+            stack.push(current);
         }
-
-        for(int i=0;i<rightParenthesesCount;i++){
-            s = "(" + s;
-        }
-
+        addParentheses(rightParenthesesCount);
+        s = stackToString();
         return s;
+    }
+
+    private String stackToString() {
+        StringBuffer string = new StringBuffer();
+        int size = stack.size();
+        for(int i=0;i<size;i++){
+            string.append(stack.pop());
+        }
+        return new String(string);
+    }
+
+    private boolean isStarOrForwardSlash(char current) {
+        return current == '*' || current == '/';
+    }
+
+    private void addParentheses(int rightParenthesesCount) {
+        for(int i=0;i<rightParenthesesCount;i++){
+            stack.push('(');
+        }
     }
 
 }
