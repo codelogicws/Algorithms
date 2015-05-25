01d71ccaf4228c4d64d36c26c949260c8cf4bb2a
codelogicws <russ@codelogic.ws>
2015-05-25 14:23:24 -0700

    Finished InfixToPostfix

--- src/test/ws/codelogic/Math/InfixToPostfixTest.java
+++ src/test/ws/codelogic/Math/InfixToPostfixTest.java
@@ -40,20 +40,20 @@ public class InfixToPostfixTest {
         assertEquals("72-5+", converter.convert("7-2+5"));
     }
 
-//    @Test
-//    public void threeDigitNumber() {
-//        assertEquals("4567+", converter.convert("4+(567)"));
-//    }
-//
-//    @Test
-//    public void multiplicationTest() {
-//        assertEquals("4567*", converter.convert("4*(567)"));
-//    }
-//
-//    @Test
-//    public void divisionTest() {
-//        assertEquals("4567/", converter.convert("4/(567)"));
-//    }
+    @Test
+    public void threeDigitNumber() {
+        assertEquals("4(567)+", converter.convert("4+567"));
+    }
+
+    @Test
+    public void multiplicationTest() {
+        assertEquals("4(567)*", converter.convert("4*567"));
+    }
+
+    @Test
+    public void divisionTest() {
+        assertEquals("4(567)/", converter.convert("4/567"));
+    }
 
     @Test
     public void multiplicationPriority() {
