9061590315fef6768117295e7712684bd35d6be3
codelogicws <russ@codelogic.ws>
2015-05-25 22:22:13 -0700

    Adding UnionFind

---
+++ src/ws/codelogic/collections/OutOfBounds.java
@@ -0,0 +1,4 @@
+package ws.codelogic.collections;
+
+public class OutOfBounds extends RuntimeException {
+}
--- src/ws/codelogic/collections/UnionFind.java
+++ src/ws/codelogic/collections/UnionFind.java
@@ -2,10 +2,30 @@ package ws.codelogic.collections;
 
 public class UnionFind {
 
-    public void connect(int index1, int index2) {
+    private int[] nodes;
+
+    public UnionFind(int size){
+        nodes = new int[size];
+        for(int i=0;i<size;i++){
+            nodes[i] = i;
+        }
     }
 
-    public boolean connected(int i, int i1) {
-        return true;
+    public void connect(int index1, int index2) {
+        indexOutOfBounds(index1, index2);
+        nodes[index1] = nodes[index2];
+    }
+
+    private boolean sizeOutOfBounds(int index) {
+        return index >= nodes.length || index < 0;
+    }
+
+    public boolean connected(int index1, int index2) {
+        indexOutOfBounds(index1, index2);
+        return nodes[index1] == nodes[index2];
+    }
+
+    private void indexOutOfBounds(int i1, int i2) {
+        if(sizeOutOfBounds(i1) || sizeOutOfBounds(i2)) throw new OutOfBounds();
     }
 }
