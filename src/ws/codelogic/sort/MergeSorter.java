c0f2b0bb3bd10761c06f4abbb1f7d0adbacb3aac
codelogicws <russ@codelogic.ws>
2015-05-26 13:27:36 -0700

    Merge sort is working.

--- src/ws/codelogic/sort/MergeSorter.java
+++ src/ws/codelogic/sort/MergeSorter.java
@@ -1,10 +1,42 @@
 package ws.codelogic.sort;
 
+import java.util.Arrays;
+
 public class MergeSorter<T extends Comparable<T>> implements Sorter<T>{
 
     @Override
     public T[] sort(T[] array) {
+        mergeSort(array, 0, array.length-1);
         return array;
+    }
+
+    private void mergeSort(T[] array, int start, int end) {
+        if(start >= end) return;
+        int mid = (start+end)/2;
+        mergeSort(array, start, mid);
+        mergeSort(array, mid+1, end);
+        merge(array, start, mid, end);
+    }
+
+    private void merge(T[] array, int start, int mid, int end) {
+        if(start == end) return;
+        T[] left = Arrays.copyOfRange(array, start, mid + 1);
+        T[] right = Arrays.copyOfRange(array, mid+1, end+1);
+        int leftPointer = 0;
+        int rightPointer = 0;
+        int mainPointer = start;
+
+        while(leftPointer < left.length || rightPointer < right.length){
+            if(leftPointer >= left.length){
+                array[mainPointer++] = right[rightPointer++];
+            }else if(rightPointer >= right.length){
+                array[mainPointer++] = left[leftPointer++];
+            }else if(left[leftPointer].compareTo(right[rightPointer]) <= 0){
+                array[mainPointer++] = left[leftPointer++];
+            }else{
+                array[mainPointer++] = right[rightPointer++];
+            }
+        }
     }
 
 }
--- test/ws/codelogic/sort/MergeSorterTest.java
+++ test/ws/codelogic/sort/MergeSorterTest.java
@@ -19,9 +19,19 @@ public class MergeSorterTest{
         assertMatch(newArray(1,2), sort(1,2));
     }
 
+    @Test
+    public void sortTheSimplest2Numbers() {
+        assertMatch(newArray(1,2), sort(2,1));
+    }
+
+    @Test
+    public void largerSortTest() {
+        assertMatch(newArray(1,5,7,34,98,965), sort(34, 5, 98, 1, 965, 7));
+    }
+
     private void assertMatch(Integer[] expected, Integer[] actual) {
         for (int i = 0; i < expected.length; i++)
-            if(expected[i].equals(actual[i]))
+            if(!expected[i].equals(actual[i]))
                 assertTrue(false);
         assertTrue(true);
     }
