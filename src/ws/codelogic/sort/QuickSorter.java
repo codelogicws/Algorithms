ace74f1621032f57d2ba7e6a55c186d8221a7342
codelogicws <russ@codelogic.ws>
2015-05-27 12:08:32 -0700

    Quick sort is working

--- src/ws/codelogic/sort/QuickSorter.java
+++ src/ws/codelogic/sort/QuickSorter.java
@@ -1,5 +1,7 @@
 package ws.codelogic.sort;
 
+import java.util.Arrays;
+
 public class QuickSorter <T extends Comparable<T>> implements Sorter<T>{
 
     @Override
@@ -9,14 +11,32 @@ public class QuickSorter <T extends Comparable<T>> implements Sorter<T>{
     }
 
     private void quickSort(T[] array, int start, int end) {
-        if(start == end) return;
-        partition(array, start, end);
-        int mid = (start+end)/2;
-        quickSort(array, start, mid);
-        quickSort(array, mid+1, end);
+        if(end-start <= 0) return;
+        T pivot = array[end];
+        int pivotLocation = partition(array, start, end, pivot);
+        quickSort(array, start, pivotLocation-1);
+        quickSort(array, pivotLocation+1, end);
+        partition(array, start, end, pivot);
     }
 
-    private void partition(T[] array, int start, int end) {
-        System.out.println("debug-QuickSorter: Start: " + start + " End: " + end);
+    private int partition(T[] array, int start, int end, T pivot) {
+        int leftPointer = start-1;
+        int rightPointer = end;
+        while(true){
+            while(array[++leftPointer].compareTo(pivot) < 0);
+            while(rightPointer > 0 && array[--rightPointer].compareTo(pivot) >= 0);
+            if(leftPointer>=rightPointer)break;
+            else swap(array, leftPointer, rightPointer);
+        }
+        if(array[leftPointer].compareTo(array[end]) > 0)swap(array, leftPointer, end);
+        return leftPointer;
+    }
+
+
+
+    private void swap(T[] array, int start, int end) {
+        T temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
     }
 }
--- test/ws/codelogic/sort/QuickSorterTest.java
+++ test/ws/codelogic/sort/QuickSorterTest.java
@@ -3,6 +3,8 @@ package ws.codelogic.sort;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class QuickSorterTest {
@@ -16,19 +18,18 @@ public class QuickSorterTest {
 
     @Test
     public void alreadySortedList_ShouldGiveListBack(){
-        sort(4,5,6,7,8,8,4,3,4,5);
-        assertMatch(newArray(1,2), sort(1,2));
+        assertMatch(newArray(1, 2), sort(1, 2));
     }
 
-//    @Test
-//    public void sortTheSimplest2Numbers() {
-//        assertMatch(newArray(1,2), sort(2,1));
-//    }
-//
-//    @Test
-//    public void largerSortTest() {
-//        assertMatch(newArray(1,5,7,34,98,965), sort(34, 5, 98, 1, 965, 7));
-//    }
+    @Test
+    public void sortTheSimplest2Numbers() {
+        assertMatch(newArray(1,2), sort(2,1));
+    }
+
+    @Test
+    public void largerSortTest() {
+        assertMatch(newArray(1,5,7,34,98,965), sort(34, 5, 98, 1, 965, 7));
+    }
 
     private void assertMatch(Integer[] expected, Integer[] actual) {
         for (int i = 0; i < expected.length; i++)
@@ -39,7 +40,7 @@ public class QuickSorterTest {
 
     private Integer[] sort(int... i) {
         return sorter.sort(newArray(i));
-    }
+}
 
     private Integer[] newArray(int... i) {
         Integer[] newArray = new Integer[i.length];
