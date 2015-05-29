5a95c7f5bf893140aee93659642502c3618dd26d
codelogicws <russ@codelogic.ws>
2015-05-28 23:12:36 -0700

    Merge sort is slightly more readable.

--- src/ws/codelogic/sort/MergeSorter.java
+++ src/ws/codelogic/sort/MergeSorter.java
@@ -4,21 +4,26 @@ import java.util.Arrays;
 
 public class MergeSorter<T extends Comparable<T>> implements Sorter<T>{
 
+    private T[] array;
+
     @Override
     public T[] sort(T[] array) {
-        mergeSort(array, 0, array.length-1);
+        //need to throw error instead;
+        if(array == null || array.length == 0)return null;
+        this.array = array;
+        mergeSort(0, array.length-1);
         return array;
     }
 
-    private void mergeSort(T[] array, int start, int end) {
+    private void mergeSort(int start, int end) {
         if(start >= end) return;
         int mid = (start+end)/2;
-        mergeSort(array, start, mid);
-        mergeSort(array, mid+1, end);
-        merge(array, start, mid, end);
+        mergeSort(start, mid);
+        mergeSort(mid+1, end);
+        merge(start, mid, end);
     }
 
-    private void merge(T[] array, int start, int mid, int end) {
+    private void merge(int start, int mid, int end) {
         if(start == end) return;
         T[] left = Arrays.copyOfRange(array, start, mid + 1);
         T[] right = Arrays.copyOfRange(array, mid+1, end+1);
