a558b95a6a35986a54e106ee4b3afb6f13d6c9af
codelogicws <russ@codelogic.ws>
2015-05-29 22:29:03 -0700

    Changed merge sort such that is doesn't initalize arrays over and over again.

--- src/ws/codelogic/sort/MergeSorter.java
+++ src/ws/codelogic/sort/MergeSorter.java
@@ -5,13 +5,15 @@ import java.util.Arrays;
 public class MergeSorter<T extends Comparable<T>> implements Sorter<T>{
 
     private T[] array;
+    private T[] temp;
 
     @Override
     public T[] sort(T[] array) {
         //need to throw error instead;
         if(array == null || array.length == 0)return null;
         this.array = array;
-        mergeSort(0, array.length-1);
+        temp = Arrays.copyOf(array, array.length);
+        mergeSort(0, array.length - 1);
         return array;
     }
 
@@ -25,22 +27,35 @@ public class MergeSorter<T extends Comparable<T>> implements Sorter<T>{
 
     private void merge(int start, int mid, int end) {
         if(start == end) return;
-        T[] left = Arrays.copyOfRange(array, start, mid + 1);
-        T[] right = Arrays.copyOfRange(array, mid+1, end+1);
-        int leftPointer = 0;
-        int rightPointer = 0;
-        int mainPointer = start;
+        copyArrayToTemp(start, end);
+        int i = start;
+        int j = mid+1;
+        int k = start;
 
-        while(leftPointer < left.length || rightPointer < right.length){
-            if(leftPointer >= left.length){
-                array[mainPointer++] = right[rightPointer++];
-            }else if(rightPointer >= right.length){
-                array[mainPointer++] = left[leftPointer++];
-            }else if(left[leftPointer].compareTo(right[rightPointer]) <= 0){
-                array[mainPointer++] = left[leftPointer++];
+        while(i<=mid && j<=end){
+            assert i <= end;
+            assert j <= end;
+            if(temp[i].compareTo(temp[j]) <= 0){
+                array[k] = temp[i];
+                i++;
             }else{
-                array[mainPointer++] = right[rightPointer++];
+                array[k] = temp[j];
+                j++;
             }
+            k++;
+        }
+
+        while(i<=mid){
+            array[k] = temp[i];
+            i++;
+            k++;
+        }
+
+    }
+
+    private void copyArrayToTemp(int start, int end) {
+        for(int i=start;i<=end;i++){
+            temp[i] = array[i];
         }
     }
 
--- test/ws/codelogic/sort/MergeSorterTest.java
+++ test/ws/codelogic/sort/MergeSorterTest.java
@@ -2,6 +2,7 @@ package ws.codelogic.sort;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MergeSorterTest{
