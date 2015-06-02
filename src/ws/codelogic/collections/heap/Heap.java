464309061ccba52cff75b8600a5068be7cb194c0
codelogicws <russ@codelogic.ws>
2015-06-01 21:20:00 -0700

    Switching computers again.

--- src/ws/codelogic/collections/heap/Heap.java
+++ src/ws/codelogic/collections/heap/Heap.java
@@ -2,7 +2,43 @@ package ws.codelogic.collections.heap;
 
 public class Heap<T extends Comparable <T>> {
 
-    public void add(T i) {
+    private T[] array;
+    private int lastElement;
+
+    public Heap(T[] array){
+        this.array = array;
+        lastElement = 0;
     }
 
+    public void insert(T element) {
+        lastElement++;
+        array[lastElement] = element;
+        moveUp(lastElement);
+    }
+
+    private void moveUp(int lastElement) {
+        if(lastElement < 2) return;
+        int parent = lastElement/2;
+        if(array[parent].compareTo(array[lastElement]) < 0){
+            swap(parent, lastElement);
+        }
+    }
+
+    private void swap(int i1, int i2) {
+        T temp = array[i2];
+        array[i2] = array[i1];
+        array[i1] = temp;
+    }
+
+    public T pop() {
+        swap(lastElement, 1);
+        moveDown(1);
+        return array[lastElement--];
+    }
+
+    private void moveDown(int i) {
+//        int child1 = i*2;
+//        int child2 = (i*2)+1;
+
+    }
 }
--- test/ws/codelogic/collections/heap/HeapTest.java
+++ test/ws/codelogic/collections/heap/HeapTest.java
@@ -3,24 +3,72 @@ package ws.codelogic.collections.heap;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class HeapTest{
 
     private Heap<Integer> heap;
 
     @Before
     public void startUp(){
-        heap = new Heap<>();
+        Integer[] array = new Integer[5];
+        heap = new Heap<>(array);
     }
 
     @Test
     public void addItemToHeap() {
-        heap.add(5);
+        heap.insert(5);
     }
 
     @Test
     public void addOneRemoveTheSame() {
-        heap.add(5);
-        System.out.println();
+        heap.insert(5);
+        assertEquals(new Integer(5), heap.pop());
+    }
+
+    @Test
+    public void add2_getHeighestNumberFirst() {
+        addToHeap(5,9);
+        assertEquals(9, pop());
+        assertEquals(5, pop());
+    }
+
+    @Test
+    public void add2ReverseOrder_getHieghestNumber() {
+        addToHeap(9,5);
+        assertEquals(9, pop());
+        assertEquals(5, pop());
+    }
+
+    @Test
+    public void add3_pullAllLargestFirst() {
+        addToHeap(1,3,2);
+        assertEquals(3, pop());
+        assertEquals(2, pop());
+        assertEquals(1, pop());
+    }
+
+    @Test
+    public void add3_pullAllLargestFirst2() {
+        addToHeap(2,3,1);
+        assertEquals(3, pop());
+        assertEquals(2, pop());
+        assertEquals(1, pop());
+    }
+
+    @Test
+    public void add2_pullAllLargestFirst2() {
+
+    }
+
+    private int pop(){
+        return heap.pop();
+    }
+
+    private void addToHeap(int... toAdd) {
+        for(int i : toAdd){
+            heap.insert(i);
+        }
     }
 
 
